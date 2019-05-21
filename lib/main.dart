0667d08c82ae822726a5154108775d843a35a023
Filip Jerga <jerga99@gmail.com>
2019-05-21 22:38:58 +0200

    2 Material App + Scaffold Widget

--- lib/main.dart
+++ lib/main.dart
@@ -3,15 +3,22 @@ import 'package:flutter/material.dart';
 void main() => runApp(MeetuperApp());
 
 
-
 class MeetuperApp extends StatelessWidget {
+  final String appTitle = 'Meetuper App';
 
   Widget build(BuildContext context) {
-    return Center(
-      child: Text(
-        'Welcome in Meetuper App!',
-        textDirection: TextDirection.ltr
-      ),
+
+    return MaterialApp(
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: Scaffold(
+        body: Center(
+          child: Text(
+            'Welcome in $appTitle!',
+            textDirection: TextDirection.ltr
+          ),
+        ),
+        appBar: AppBar(title: Text(appTitle)),
+      )
     );
   }
 }
