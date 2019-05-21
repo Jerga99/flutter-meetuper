a7bbc7d36fa1fadf36851e29bad7e27017226a35
Filip Jerga <jerga99@gmail.com>
2019-05-21 23:33:27 +0200

    3 Columns + Separate Widget

--- lib/main.dart
+++ lib/main.dart
@@ -10,15 +10,37 @@ class MeetuperApp extends StatelessWidget {
 
     return MaterialApp(
       theme: ThemeData(primarySwatch: Colors.blue),
-      home: Scaffold(
-        body: Center(
-          child: Text(
-            'Welcome in $appTitle!',
-            textDirection: TextDirection.ltr
-          ),
-        ),
-        appBar: AppBar(title: Text(appTitle)),
-      )
+      home: CounterHomeScreen(title: appTitle)
     );
   }
 }
+
+class CounterHomeScreen extends StatelessWidget {
+  final String _title;
+
+  CounterHomeScreen({String title}): _title = title;
+
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'Welcome in $_title, lets increment numbers!',
+              textDirection: TextDirection.ltr,
+              style: TextStyle(fontSize: 15.0)
+            ),
+            Text(
+              'Click Counter: 0',
+              textDirection: TextDirection.ltr,
+              style: TextStyle(fontSize: 30.0)
+            ),
+          ],
+        )
+      ),
+      appBar: AppBar(title: Text(_title)),
+    );
+  }
+}
+
