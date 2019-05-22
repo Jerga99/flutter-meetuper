848c19a6386de5391a824d275eead5282b503d1b
Filip Jerga <jerga99@gmail.com>
2019-05-22 19:56:11 +0200

    14 Send a request to json placeholder API

--- lib/src/screens/post_screen.dart
+++ lib/src/screens/post_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_meetuper/src/widgets/bottom_navigation.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 class PostScreen extends StatefulWidget {
   @override
@@ -7,8 +9,22 @@ class PostScreen extends StatefulWidget {
 }
 
 class _PostScreenState extends State<PostScreen> {
+  List<dynamic> _posts = [];
+
+  void initState() {
+    super.initState();
+
+    http.get('https://jsonplaceholder.typicode.com/posts')
+      .then((res) {
+        print(res.body);
+        final posts = json.decode(res.body);
+        print(posts);
+        setState(() => _posts = posts);
+      });
+  }
 
   Widget build(BuildContext context) {
+    print('I am calling Build!');
     return Scaffold(
       body: Center(
         child: Column(
