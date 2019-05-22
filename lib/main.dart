6749a5c12a0fb039a0f9d20e8137f165f160572e
Filip Jerga <jerga99@gmail.com>
2019-05-22 11:28:16 +0200

    6 Counter Home Screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_meetuper/src/screens/counter_home_screen.dart';
 
 void main() => runApp(MeetuperApp());
-
 
 class MeetuperApp extends StatelessWidget {
   final String appTitle = 'Meetuper App';
@@ -15,42 +15,5 @@ class MeetuperApp extends StatelessWidget {
   }
 }
 
-class CounterHomeScreen extends StatelessWidget {
-  final String _title;
-  final int _counter = 0;
 
-  CounterHomeScreen({String title}): _title = title;
-
-  _increment() {
-    print('I am clicking floating button!');
-  }
-
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'Welcome in $_title, lets increment numbers!',
-              textDirection: TextDirection.ltr,
-              style: TextStyle(fontSize: 15.0)
-            ),
-            Text(
-              'Counter: $_counter',
-              textDirection: TextDirection.ltr,
-              style: TextStyle(fontSize: 30.0)
-            ),
-          ],
-        )
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _increment,
-        tooltip: 'Increment',
-        child: Icon(Icons.add)
-      ),
-      appBar: AppBar(title: Text(_title)),
-    );
-  }
-}
 
---
+++ lib/src/screens/counter_home_screen.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+
+class CounterHomeScreen extends StatelessWidget {
+  final String _title;
+  final int _counter = 0;
+
+  CounterHomeScreen({String title}): _title = title;
+
+  _increment() {
+    print('I am clicking floating button!');
+  }
+
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'Welcome in $_title, lets increment numbers!',
+              textDirection: TextDirection.ltr,
+              style: TextStyle(fontSize: 15.0)
+            ),
+            Text(
+              'Counter: $_counter',
+              textDirection: TextDirection.ltr,
+              style: TextStyle(fontSize: 30.0)
+            ),
+          ],
+        )
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _increment,
+        tooltip: 'Increment',
+        child: Icon(Icons.add)
+      ),
+      appBar: AppBar(title: Text(_title)),
+    );
+  }
+}
