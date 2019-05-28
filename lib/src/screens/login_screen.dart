d0a182376fdfe598053dcd90be0ead563b27936d
Filip Jerga <jerga99@gmail.com>
2019-05-28 10:29:03 +0200

    55 Login Form Inputs

--- lib/src/screens/login_screen.dart
+++ lib/src/screens/login_screen.dart
@@ -12,9 +12,47 @@ class _LoginScreenState extends State<LoginScreen> {
 
   Widget build(BuildContext context) {
     return Scaffold(
-      body: FlatButton(
-        child: Text('Go To Register Screen'),
-        onPressed: () => Navigator.pushNamed(context, RegisterScreen.route),
+      body: Padding(
+        padding: EdgeInsets.all(20.0),
+        child: Form(
+          // Provide key
+          child: Column(
+            children: [
+              Container(
+                margin: EdgeInsets.only(bottom: 15.0),
+                child: Text(
+                  'Login And Explore',
+                  style: TextStyle(
+                    fontSize: 30.0,
+                    fontWeight: FontWeight.bold
+                  ),
+                ),
+              ),
+              TextFormField(
+                style: Theme.of(context).textTheme.headline,
+                decoration: InputDecoration(
+                  hintText: 'Email Address'
+                ),
+              ),
+              TextFormField(
+                style: Theme.of(context).textTheme.headline,
+                decoration: InputDecoration(
+                  hintText: 'Password'
+                ),
+              ),
+              Container(
+                alignment: Alignment(-1.0, 0.0),
+                margin: EdgeInsets.only(top: 10.0),
+                child: RaisedButton(
+                  textColor: Colors.white,
+                  color: Theme.of(context).primaryColor,
+                  child: const Text('Submit'),
+                  onPressed: () => {},
+                )
+              )
+            ],
+          ),
+        )
       ),
       appBar: AppBar(title: Text('Login')),
     );
---
+++ resources/login_form.dart
@@ -0,0 +1,32 @@
+Padding(
+  padding: EdgeInsets.all(20.0),
+  child: Form(
+    // Provide key
+    child: Column(
+      children: [
+        Container(
+          margin: EdgeInsets.only(bottom: 15.0),
+          child: Text(
+            'Login And Explore',
+            style: TextStyle(
+              fontSize: 30.0,
+              fontWeight: FontWeight.bold
+            ),
+          ),
+        ),
+        // TextFormField
+        // TextFormField
+        Container(
+          alignment: Alignment(-1.0, 0.0),
+          margin: EdgeInsets.only(top: 10.0),
+          child: RaisedButton(
+            textColor: Colors.white,
+            color: Theme.of(context).primaryColor,
+            child: const Text('Submit'),
+            onPressed: () => {},
+          )
+        )
+      ],
+    ),
+  )
+)
