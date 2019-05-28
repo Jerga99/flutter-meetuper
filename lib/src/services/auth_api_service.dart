fc3ca0f6db3c5a7793b78f2d62e8faddf12503d7
Filip Jerga <jerga99@gmail.com>
2019-05-28 21:37:25 +0200

    69 Store Token

--- lib/src/services/auth_api_service.dart
+++ lib/src/services/auth_api_service.dart
@@ -8,12 +8,23 @@ import 'dart:io' show Platform;
 
 class AuthApiService {
   final String url = Platform.isIOS ? 'http://localhost:3001/api/v1' : 'http://10.0.2.2:3001/api/v1';
+  String _token;
+
   static final AuthApiService _singleton = AuthApiService._internal();
 
   factory AuthApiService() {
     return _singleton;
   }
   AuthApiService._internal();
+
+  bool _saveToken(String token) {
+    if (token != null) {
+      _token = token;
+      return true;
+    }
+
+    return false;
+  }
 
   Future<Map<String, dynamic>> login(LoginFormData loginData) async {
     final body = json.encode(loginData.toJSON());
@@ -24,6 +35,8 @@ class AuthApiService {
     final parsedData = Map<String, dynamic>.from(json.decode(res.body));
 
     if (res.statusCode == 200) {
+      _saveToken(parsedData['token']);
+      print(_token);
       return parsedData;
     } else {
       return Future.error(parsedData);
