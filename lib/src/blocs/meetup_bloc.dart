735213527f455f5f4ea3c92710997f3b301f3edd
Filip Jerga <jerga99@gmail.com>
2019-06-03 23:27:19 +0200

    116 Meetup Bloc - Join and Leave Meetup

--- lib/src/blocs/meetup_bloc.dart
+++ lib/src/blocs/meetup_bloc.dart
@@ -2,10 +2,14 @@ import 'dart:async';
 
 import 'package:flutter_meetuper/src/blocs/bloc_provider.dart';
 import 'package:flutter_meetuper/src/models/meetup.dart';
+import 'package:flutter_meetuper/src/models/user.dart';
+import 'package:flutter_meetuper/src/services/auth_api_service.dart';
 import 'package:flutter_meetuper/src/services/meetup_api_service.dart';
 
 class MeetupBloc implements BlocBase {
+  // Better idea would be to inject services in constructor
   final MeetupApiService _api = MeetupApiService();
+  final AuthApiService _auth = AuthApiService();
 
   final StreamController<List<Meetup>> _meetupController = StreamController.broadcast();
   Stream<List<Meetup>> get meetups => _meetupController.stream;
@@ -23,6 +27,32 @@ class MeetupBloc implements BlocBase {
    void fetchMeetup(String meetupId) async {
     final meetup = await _api.fetchMeetupById(meetupId);
     _inMeetup.add(meetup);
+  }
+
+  void joinMeetup(Meetup meetup) {
+    _api.joinMeetup(meetup.id)
+      .then((_) {
+        User user = _auth.authUser;
+        user.joinedMeetups.add(meetup.id);
+
+        meetup.joinedPeople.add(user);
+        meetup.joinedPeopleCount++;
+        _inMeetup.add(meetup);
+      })
+      .catchError((err) => print(err));
+  }
+
+  void leaveMeetup(Meetup meetup) {
+    _api.leaveMeetup(meetup.id)
+      .then((_) {
+        User user = _auth.authUser;
+        user.joinedMeetups.removeWhere((jMeetup) => jMeetup == meetup.id);
+
+        meetup.joinedPeople.removeWhere((jUser) => jUser.id == user.id);
+        meetup.joinedPeopleCount--;
+        _inMeetup.add(meetup);
+      })
+      .catchError((err) => print(err));
   }
 
   void dispose() {
