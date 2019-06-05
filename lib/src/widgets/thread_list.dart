fbca645b4b1402e6b55c94261cb293166a64b639
Filip Jerga <jerga99@gmail.com>
2019-06-05 11:44:45 +0200

    139 Style Threads

--- lib/src/widgets/thread_list.dart
+++ lib/src/widgets/thread_list.dart
@@ -3,7 +3,9 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_meetuper/src/blocs/meetup_bloc.dart';
+import 'package:flutter_meetuper/src/models/post.dart';
 import 'package:flutter_meetuper/src/models/thread.dart';
+import 'package:timeago/timeago.dart' as timeago;
 
 class ThreadList extends StatelessWidget {
 
@@ -17,15 +19,94 @@ class ThreadList extends StatelessWidget {
       builder: (BuildContext context, AsyncSnapshot<List<Thread>> snapshot) {
         if (snapshot.hasData) {
           final threads = snapshot.data;
-          return ListView(
-            children: threads.map((thread) => Text(thread.title)).toList()
-          );
+          if (threads != null && threads.length > 0) {
+            return ListView.builder(
+              itemCount: threads.length,
+              itemBuilder: (context, i) {
+                return Card(
+                  child: Padding(
+                    padding: const EdgeInsets.all(16.0),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: <Widget>[
+                        Row(
+                          mainAxisSize: MainAxisSize.max,
+                          children: <Widget>[
+                            Padding(
+                              padding: EdgeInsets.only(right: 10.0),
+                              child: CircleAvatar(
+                                radius: 20.0,
+                                backgroundImage: NetworkImage(threads[i].user.avatar),
+                              ),
+                            ),
+                            Expanded(
+                              child: Text(threads[i].title, style: TextStyle(fontSize: 22.0)),
+                            )
+                          ],
+                        ),
+                        _PostList(posts: threads[i].posts)
+                      ],
+                    ),
+                  ),
+                );
+              },
+            );
+          } else {
+            return Center(
+              child: Text('No Threads Yet :(', style: TextStyle(fontSize: 30.0)),
+            );
+          }
         } else {
           return Center(
-            child: CircularProgressIndicator()
+            child: CircularProgressIndicator(),
           );
         }
       }
     );
   }
 }
+
+
+class _PostList extends StatelessWidget {
+  final List<Post> posts;
+
+  _PostList({@required this.posts});
+
+  String _timeUntil(DateTime date) {
+    return timeago.format(date, locale: 'en', allowFromNow: true);
+  }
+
+  Widget build(BuildContext context) {
+    if(posts != null && posts.length > 0) {
+      return Column(
+        children: posts.map((Post post) {
+          return Container(
+            padding: EdgeInsets.only(left: 10.0, top: 5.0),
+            child: Row(
+              children: <Widget>[
+                CircleAvatar(
+                  radius: 20.0,
+                  backgroundImage: NetworkImage(post.user.avatar),
+                ),
+                Padding(
+                  padding: EdgeInsets.only(left: 10.0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      Text(_timeUntil(DateTime.parse(post.updatedAt)), style: TextStyle(fontSize: 12.0, color: Colors.black54)),
+                      Text(post.text, style: TextStyle(fontSize: 16.0, color: Colors.black87))
+                    ],
+                  )
+                )
+
+              ],
+            ),
+          );
+        }).toList()
+      );
+    }
+    return Text('No Posts Yet!');
+  }
+}
+
---
+++ resources/thread_layout.dart
@@ -0,0 +1,88 @@
+// if (snapshot.hasData) {
+//   final threads = snapshot.data;
+//   if (threads != null && threads.length > 0) {
+//     return ListView.builder(
+//       itemCount: threads.length,
+//       itemBuilder: (context, i) {
+//         return Card(
+//           child: Padding(
+//             padding: const EdgeInsets.all(16.0),
+//             child: Column(
+//               mainAxisAlignment: MainAxisAlignment.start,
+//               crossAxisAlignment: CrossAxisAlignment.start,
+//               children: <Widget>[
+//                 Row(
+//                   mainAxisSize: MainAxisSize.max,
+//                   children: <Widget>[
+//                     Padding(
+//                       padding: EdgeInsets.only(right: 10.0),
+//                       child: CircleAvatar(
+//                         radius: 20.0,
+//                         backgroundImage: NetworkImage(threads[i].user.avatar),
+//                       ),
+//                     ),
+//                     Expanded(
+//                       child: Text(threads[i].title, style: TextStyle(fontSize: 22.0)),
+//                     )
+//                   ],
+//                 ),
+//                 _PostList(posts: threads[i].posts)
+//               ],
+//             ),
+//           ),
+//         );
+//       },
+//     );
+//   } else {
+//     return Center(
+//       child: Text('No Threads Yet :(', style: TextStyle(fontSize: 30.0)),
+//     );
+//   }
+// } else {
+//   return Center(
+//     child: CircularProgressIndicator(),
+//   );
+// }
+
+
+// class _PostList extends StatelessWidget {
+//   final List<Post> posts;
+
+//   _PostList({@required this.posts});
+
+//   String _timeUntil(DateTime date) {
+//     return timeago.format(date, locale: 'en', allowFromNow: true);
+//   }
+
+//   Widget build(BuildContext context) {
+//     if(posts != null && posts.length > 0) {
+//       return Column(
+//         children: posts.map((Post post) {
+//           return Container(
+//             padding: EdgeInsets.only(left: 10.0, top: 5.0),
+//             child: Row(
+//               children: <Widget>[
+//                 CircleAvatar(
+//                   radius: 20.0,
+//                   backgroundImage: NetworkImage(post.user.avatar),
+//                 ),
+//                 Padding(
+//                   padding: EdgeInsets.only(left: 10.0),
+//                   child: Column(
+//                     crossAxisAlignment: CrossAxisAlignment.start,
+//                     children: <Widget>[
+//                       Text(_timeUntil(DateTime.parse(post.updatedAt)), style: TextStyle(fontSize: 12.0, color: Colors.black54)),
+//                       Text(post.text, style: TextStyle(fontSize: 16.0, color: Colors.black87))
+//                     ],
+//                   )
+//                 )
+
+//               ],
+//             ),
+//           );
+//         }).toList()
+//       );
+//     }
+//     return Text('No Posts Yet!');
+//   }
+// }
